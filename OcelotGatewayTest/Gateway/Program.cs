a7d2efb874bb3768a3a02d056b1bd1d478693f11
Kamyab7 <valipourkamyab.official@gmail.com>
2023-11-17 00:31:39 +0330

    Config Ocelot

--- OcelotGatewayTest/Gateway/Program.cs
+++ OcelotGatewayTest/Gateway/Program.cs
@@ -7,17 +7,12 @@ builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange
 builder.Services.AddOcelot(builder.Configuration);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
