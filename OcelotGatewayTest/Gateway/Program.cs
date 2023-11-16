a6bfa9abffed352cde2e88cf0c5abd0c2801131d
Kamyab7 <valipourkamyab.official@gmail.com>
2023-11-17 00:36:30 +0330

    Fix Ocelot config

--- OcelotGatewayTest/Gateway/Program.cs
+++ OcelotGatewayTest/Gateway/Program.cs
@@ -1,4 +1,5 @@
 using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +17,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseOcelot().Wait();
 
 app.UseAuthorization();
 
