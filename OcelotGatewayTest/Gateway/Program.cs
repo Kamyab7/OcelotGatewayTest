48f5553f8a24f506688336e211e387ba05b5aaa7
Kamyab7 <valipourkamyab.official@gmail.com>
2023-11-17 21:21:52 +0330

    Config Swagger for Ocelot

--- OcelotGatewayTest/Gateway/Program.cs
+++ OcelotGatewayTest/Gateway/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Configuration;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
 builder.Services.AddControllers();
 
@@ -14,6 +16,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwaggerForOcelotUI(opt =>
+    {
+        opt.PathToSwaggerGenerator = "/swagger/docs";
+    });
 }
 
 app.UseHttpsRedirection();
